5d9a12c62585f35a26d48f3a573620f77d86785a
Anu-suresh99 <anusuresh359@gmail.com>
2023-11-14 07:24:12 -0500

    initial

---
+++ src/components/First.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Left from '@/app/Left/page'
+import Chat from '@/components/Chat'
+
+const First = () => {
+    return (
+        <div className='flex ml-[260px] bg-black mt-[40px] mr-[40px] mb-[40px] h-[1000px] rounded-[8px]'>
+            <div className=' w-[750px] border-r-2 border-gray-400 mt-5 mb-5 '>
+                <Left />
+            </div>
+            <div className='w-full '>
+                <Chat />
+            </div>
+        </div>
+    )
+}
+
+export default First
